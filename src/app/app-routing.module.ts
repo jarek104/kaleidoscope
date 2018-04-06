115e5e3364c5c4cdab04595834087dfa1ad4c6a2
jarek104 <jarek104@gmail.com>
2018-04-05 21:34:37 -0400

    need to add flex to shared modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'view/:id', component: QAVComponent},
+  // { path: 'data', component: DataTableComponent},
+  // { path: '' , redirectTo: '/data', pathMatch: 'full'},
+  // { path: '**' , component: DataTableComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/shared/shared.module.ts
@@ -0,0 +1,10 @@
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  exports: [CommonModule, MaterialModule, RouterModule],
+})
+export class SharedModule {}
