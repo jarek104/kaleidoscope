604e8f199bea00f23be5664472c6aaecb1b18626
jarek104 <jarek104@gmail.com>
2018-04-09 23:10:40 -0400

    keywords table

--- src/app/keywords-table/keywords-table.component.ts
+++ src/app/keywords-table/keywords-table.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterViewInit, Input, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { IDocument } from '../../models/document';
+import { DocumentService } from '../services/document.service';
 
 @Component({
   selector: 'app-keywords-table',
   templateUrl: './keywords-table.component.html',
   styleUrls: ['./keywords-table.component.scss']
 })
-export class KeywordsTableComponent implements OnInit {
+export class KeywordsTableComponent implements OnInit, AfterViewInit, OnChanges {
 
-  constructor() { }
+  displayedColumns = [
+    'employee_first_name',
+    'employee_last_name',
+    'email',
+    'department',
+    'city',
+    'streetAddress',
+    'stockIndustry',
+    'movieGender'];
+  dataSource = new MatTableDataSource<IDocument>();
+  selectedRow: Number;
+  setClickedRow: Function;
+  documentToEdit;
+  @Input() filterValue = '';
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private documentsService: DocumentService ) {
+    this.setClickedRow = function(index, data){
+      if (index === this.selectedRow) {
+        this.selectedRow = -1;
+        this.documentToEdit = 0;
+      } else {
+        this.selectedRow = index;
+        this.documentToEdit = data.documentID;
+      }
+    };
+  }
 
   ngOnInit() {
+    this.selectedRow = -1;
+    this.documentsService.getDocuments().subscribe(data => {
+      this.dataSource.data = data;
+
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+  ngOnChanges() {
+    this.applyFilter(this.filterValue);
+  }
+  // openRandom() {
+  //   const rand = Math.floor(Math.random() * this.dataSource.data.length);
+  //   window.open(this.dataSource.data[rand].Link);
+  // }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
   }
 
 }
--- src/models/document.ts
+++ src/models/document.ts
@@ -2,6 +2,15 @@ export interface IDocument {
     id: string;
     name: string;
     author: string;
-    dateCreated: string;
-    lastModified: string;
+    dateCreated: Date;
+    lastModified: Date;
+    employee_first_name: string;
+    employee_last_name: string;
+    email: string;
+    department: string;
+    city: string;
+    streetAddress: string;
+    stockIndustry: string;
+    movieGender: string;
+    imageUrl: string;
 }
