a02dc9341c3c6becade2e6073385bac271bdf795
jarek104 <jarek104@gmail.com>
2018-04-14 12:27:48 -0400

    enum

---
+++ src/app/kaleidoscope/models/view.ts
@@ -0,0 +1,6 @@
+enum ViewType {
+    Metadata = 'metadata',
+    Keywords = 'keywords',
+    Thumbnails = 'thumbnails',
+    Map = 'map'
+}
--- src/app/kaleidoscope/services/table-view.service.ts
+++ src/app/kaleidoscope/services/table-view.service.ts
@@ -4,10 +4,16 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class TableViewService {
 
-  private selectedView = new BehaviorSubject<string>('metadata');
+  private selectedView = new BehaviorSubject<string>(ViewType.Metadata);
   selectedView$ = this.selectedView.asObservable();
 
-  setCurrentView(view: string) {
+  setCurrentView(view: ViewType) {
     this.selectedView.next(view);
   }
+}
+enum ViewType {
+  Metadata = 'metadata',
+  Keywords = 'keywords',
+  Thumbnails = 'thumbnails',
+  Map = 'map'
 }
