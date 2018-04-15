85345f25737f6ea6437eaec236bb9ff17168bcc4
jarek104 <jarek104@gmail.com>
2018-04-15 12:37:31 -0400

    filtered selection works

--- src/app/kaleidoscope/views/keywords-table/keywords-table.component.ts
+++ src/app/kaleidoscope/views/keywords-table/keywords-table.component.ts
@@ -45,13 +45,13 @@ export class KeywordsTableComponent implements OnInit, AfterViewInit, OnChanges 
 
   isAllSelected() {
     const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
+    const numRows = this.dataSource.filteredData.length;
+    return numSelected >= numRows;
   }
   masterToggle() {
     this.isAllSelected() ?
         this.selection.clear() :
-        this.dataSource.data.forEach(row => this.selection.select(row));
+        this.dataSource.filteredData.forEach(row => this.selection.select(row));
   }
 
   ngAfterViewInit() {
